5388cea9a525654da20ce09661c295d98e87e867
Mike Attara <mpyebattara@gmail.com>
2024-08-17 21:27:18 +0000

    update

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -24,7 +24,7 @@ class Library(models.Model):
         return self.name
 
 
-class Liberian(models.Model):
+class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(
         Library, on_delete=models.CASCADE, related_name="librarian")
