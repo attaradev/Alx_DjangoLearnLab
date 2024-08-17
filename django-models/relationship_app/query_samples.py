14457abe8b37fa10b6499988bbaf3b148a90c49e
Mike Attara <mpyebattara@gmail.com>
2024-08-17 21:28:53 +0000

    update

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Book, Library
 books = Book.objects.filter(author__name="J.K. Rowling")
 
 # List all books in a library.
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name=library_name)
 books = library.books.all()
 
 # Retrieve the librarian for a library.
