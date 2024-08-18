73fa370aa2c5e0bdf06a16e937b65b6b8cb8c83e
Mike Attara <mpyebattara@gmail.com>
2024-08-18 05:05:45 +0000

    add views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Book, Library
 
-# Create your views here.
+
+def book_list_view(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.book_set.all()
+        return context
