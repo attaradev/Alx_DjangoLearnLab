6360b6da6ae51c8cc6e2eaebe6997133c847e92b
Mike Attara <mpyebattara@gmail.com>
2024-08-21 11:37:38 +0000

    add permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth.decorators import permission_required
 # Create your views here.
 
 
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    return render(request, 'bookshelf/book_list.html')
+
+
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request):
     return render(request, 'bookshelf/edit_book.html')
