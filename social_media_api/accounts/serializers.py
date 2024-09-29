fa447f92b51d14b91d56284250db11540fe55939
Mike Attara <mpyebattara@gmail.com>
2024-09-29 23:50:00 +0000

    update

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,15 +9,25 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'bio', 'profile_picture')
+        # Ensure password is write-only
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = get_user_model().objects.create(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-        Token.objects.create(user=user)  # Create token for the new user
+        # Use create_user to ensure the password is hashed
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)  # Create a token for the new user
         return user
 
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+    def validate(self, attrs):
+        # Perform custom validation for login
+        user = authenticate(
+            username=attrs['username'], password=attrs['password'])
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+        attrs['user'] = user
+        return attrs
