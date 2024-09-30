7db2090a3abfd450be079c085e5ae91a0c016cd9
Mike Attara <mpyebattara@gmail.com>
2024-09-30 00:01:01 +0000

    add posts

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(
+        read_only=True)  # Display author username
+
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'title', 'content',
+                  'created_at', 'updated_at')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(
+        read_only=True)  # Display author username
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'post', 'author', 'content',
+                  'created_at', 'updated_at')
---
+++ social_media_api/posts/views.py
@@ -0,0 +1,37 @@
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from django_filters.rest_framework import FilterSet, filters
+
+
+class PostFilter(FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Post
+        fields = ['title']
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filterset_class = PostFilter
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(author=self.request.user)
