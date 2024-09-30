0abdd7a2edbdd6b164637b5633d81a7ae2c2c93e
Mike Attara <mpyebattara@gmail.com>
2024-09-30 11:49:59 +0000

    update

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from django_filters.rest_framework import FilterSet, filters
+from django.contrib.auth import get_user_model
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from django_filters.rest_framework import FilterSet, filters
+
+User = get_user_model()
 
 
 class PostFilter(FilterSet):
@@ -18,7 +20,7 @@ class PostFilter(FilterSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     filterset_class = PostFilter
 
     def perform_create(self, serializer):
@@ -31,7 +33,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -41,11 +43,16 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 
 class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
+        # Get the users the current user is following
         following_users = request.user.following.all()
+
+        # Get the posts by the users followed by the current user, ordered by creation time
         posts = Post.objects.filter(
             author__in=following_users).order_by('-created_at')
+
+        # Serialize the posts and return the response
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
